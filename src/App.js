f975e74a9b3ba23f6c64f30ff0f91cf4a0106527
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-30 17:33:00 +0400

    feat: change the board display from flex matrix to simple grid with 100 cols

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 const screenWidth = window.screen.width;
 const screenHeight = window.screen.height;
 const squareWidth = screenWidth / 100;
@@ -8,20 +9,15 @@ const numberOfSquares = numberOfRows * 100;
 
 const board = Array.from(Array(numberOfSquares).keys());
 
-const groupArray = board.reduce((acc, item, index) => {
-  let currentIndex = index % (numberOfSquares/100);
-  (acc[currentIndex] = acc[currentIndex] || []).push(item);
-  return acc;
-}, []);
-
 function App() {
+
   return (
     <div className="App">
-      {groupArray.map((row) => (
-        <div key={Math.random()} className="row">
-          {row.map((square) => (
-            <div key={square} className="square">{square}</div>
-          ))}
+      {board.map((square) => (
+        <div key={square} className="square">
+          <div>
+            {square}
+          </div>
         </div>
       ))}
     </div>
@@ -29,3 +25,19 @@ function App() {
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {groupArray.map((row) => (
+//         <div key={Math.random()} className="row">
+//           {row.map((square) => (
+//             <div key={square} className="square">{square}</div>
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default App;
