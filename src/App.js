4569336138916c852d9bcbd48e99fc6460d4b0f9
AvetBadalyan <avetbadalyan@gmail.com>
2022-07-30 17:18:29 +0400

    feat: calculate the number of cells to fill the screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import './App.css';
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+const squareWidth = screenWidth / 100;
+let numberOfRows = screenHeight / squareWidth;
+numberOfRows = Math.floor(numberOfRows / 10) * 10;
+const numberOfSquares = numberOfRows * 100;
+
+const board = Array.from(Array(numberOfSquares).keys());
 
 function App() {
   return (
