cd7ec8c776dddc4aab2c62eebbb3e379acf986f6
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-04 01:17:45 +0400

    feat: add a pop-up to choose a color

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Cell from "./Component/Cell";
 const screenWidth = window.screen.width;
@@ -10,11 +11,40 @@ const numberOfSquares = numberOfRows * 100;
 const board = Array.from(Array(numberOfSquares).keys());
 
 function App() {
+  // Show or hide the custom context menu
+  const [isShown, setIsShown] = useState(false);
+
+  // Show the custom context menu
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setIsShown(true);
+  };
+
+  const hideContextMenu = (e) => {
+    setIsShown(false);
+  };
+
+  const paintBlue = () => {
+
+  }
   return (
-    <div className="App">
+    <div
+      onContextMenu={showContextMenu}
+      onClick={hideContextMenu}
+      className="App"
+    >
       {board.map((square) => (
         <Cell key={square} square={square} />
       ))}
+      {isShown && (
+        <div className="pop-up">
+          <button className="color-choose blue"></button>
+          <button className="color-choose yellow"></button>
+          <button className="color-choose red"></button>
+          <button className="color-choose green"> </button>
+          <button className="color-choose black"></button>
+        </div>
+      )}
     </div>
   );
 }
--- src/Component/Cell.jsx
+++ src/Component/Cell.jsx
@@ -3,22 +3,20 @@ import { useState } from "react";
 import "./../App.css";
 
 export default function Cell({ square }) {
-    const [painted, setPainted] = useState(false);
-    
+  const [painted, setPainted] = useState(false);
   const handlePainting = () => {
     setPainted((prev) => !prev);
-  }
-  const  handleDragOver = (e) => {
+  };
+  const handleDragOver = (e) => {
     e.preventDefault();
     setPainted(true);
-  }
+  };
   return (
     <div
       onClick={handlePainting}
       onDragOver={handleDragOver}
       className={painted ? "square painted" : "square"}
-    >
       
-    </div>
+    ></div>
   );
 }
