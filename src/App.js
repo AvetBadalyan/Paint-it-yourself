4f9f75cee1b3099d17b887e329296d99f0d9d33d
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-03 19:48:16 +0400

    feat: simplify the code to optimize performance

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./App.css";
+import Cell from "./Component/Cell";
 const screenWidth = window.screen.width;
 const screenHeight = window.screen.height;
 const squareWidth = screenWidth / 100;
@@ -10,44 +10,13 @@ const numberOfSquares = numberOfRows * 100;
 const board = Array.from(Array(numberOfSquares).keys());
 
 function App() {
-  const [paintedSquares, setPaintedSquares] = useState(new Set());
-  const paint = (id) => {
-    let newPainted = new Set([...paintedSquares]);
-    if (newPainted.has(id)) {
-      newPainted.delete(id);
-    } else {
-      newPainted.add(id);
-    }
-    setPaintedSquares(newPainted);
-  };
-
   return (
     <div className="App">
       {board.map((square) => (
-        <div key={square} className="square" onClick={() => paint(square)}>
-          <div className={paintedSquares.has(square) ? "painted" : ""}>
-            {square}
-          </div>
-        </div>
+        <Cell key={square} square={square} />
       ))}
     </div>
   );
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {groupArray.map((row) => (
-//         <div key={Math.random()} className="row">
-//           {row.map((square) => (
-//             <div key={square} className="square">{square}</div>
-//           ))}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default App;
