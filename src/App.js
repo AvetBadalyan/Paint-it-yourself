19cfb2b696c07f6ca7d78350eb3785a242b09f05
AvetBadalyan <avetbadalyan@gmail.com>
2022-08-04 02:03:35 +0400

    feat: replace states from app to cell

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import Cell from "./Component/Cell";
 const screenWidth = window.screen.width;
@@ -10,45 +9,16 @@ const numberOfSquares = numberOfRows * 100;
 
 const board = Array.from(Array(numberOfSquares).keys());
 
-function App() {
-  // Show or hide the custom context menu
-  const [isShown, setIsShown] = useState(false);
-  const [color, setColor] = useState("purple");
-
-  // Show the custom context menu
-  const showContextMenu = (e) => {
-    e.preventDefault();
-    setIsShown(true);
-  };
-
-  const hideContextMenu = (e) => {
-    setIsShown(false);
-  };
-
-  const colors = ["blue", "yellow", "red", "green", "black"];
-
-  const paintColor = (color) => {
-    setColor(color);
-  };
+function App() {  
   return (
     <div
-      onContextMenu={showContextMenu}
-      onClick={hideContextMenu}
+
       className="App"
     >
       {board.map((square) => (
-        <Cell color={color} key={square} square={square} />
+        <Cell key={square} square={square} />
       ))}
-      {isShown && (
-        <div className="pop-up">
-          {colors.map((color) => (
-            <button
-              onClick={() => paintColor(color)}
-              className={`color-choose ${color}`}
-            ></button>
-          ))}
-        </div>
-      )}
+
     </div>
   );
 }
--- src/Component/Cell.jsx
+++ src/Component/Cell.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import { useState } from "react";
 import "./../App.css";
 
-export default function Cell({ square, color }) {
+export default function Cell({ square }) {
   const [painted, setPainted] = useState(false);
+  // Show or hide the custom context menu
+  const [isShown, setIsShown] = useState(false);
+  const [color, setColor] = useState("blue");
+
+  // Show the custom context menu
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setIsShown(true);
+  };
+
+  const hideContextMenu = (e) => {
+    setIsShown(false);
+  };
+
+  const colors = ["blue", "yellow", "red", "green", "black"];
+
+  const paintColor = (color) => {
+    setColor(color);
+  };
+
   const handlePainting = () => {
     setPainted((prev) => !prev);
   };
@@ -13,9 +33,24 @@ export default function Cell({ square, color }) {
   };
   return (
     <div
-      onClick={handlePainting}
+      onClick={() => {
+        handlePainting();
+        hideContextMenu();
+      }}
       onDragOver={handleDragOver}
       className={painted ? `square ${color}` : "square"}
-    ></div>
+      onContextMenu={showContextMenu}
+    >
+      {isShown && (
+        <div className="pop-up">
+          {colors.map((color) => (
+            <button
+              className={`color-choose ${color}`}
+              onClick={() => paintColor(color)}
+            ></button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
